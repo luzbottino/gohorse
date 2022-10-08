c1fa166d9535e71fc54b24eaac5e90147a4ed1c3
luis.f.bottino@gmail.com <luis.f.bottino@gmail.com>
2022-10-07 22:24:56 -0300

    alterado a mensagem de entrada

--- src/main/java/br/com/estabulo/Main.java
+++ src/main/java/br/com/estabulo/Main.java
@@ -2,6 +2,6 @@ package br.com.estabulo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Seja bem vindo ao estábulo");
     }
 }
