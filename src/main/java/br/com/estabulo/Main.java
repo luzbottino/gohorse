3eeb048a1444cf4575b79034cbc2614a7a96c617
Luiz Bottino <luizaugustobottiino@gmail.com>
2022-10-07 22:30:41 -0300

    nova mensagem na main

--- src/main/java/br/com/estabulo/Main.java
+++ src/main/java/br/com/estabulo/Main.java
@@ -3,5 +3,7 @@ package br.com.estabulo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Nova mensagem aqui!");
     }
+
 }
