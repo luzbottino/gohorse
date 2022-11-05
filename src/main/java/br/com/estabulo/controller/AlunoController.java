d2d5f0181a3c415d60c905e37be7371a03c381ac
Luis Bottino <luis.bottino@terceiros.sicredi.com.br>
2022-11-04 21:35:36 -0300

    voltando da base.

--- src/main/java/br/com/estabulo/controller/AlunoController.java
+++ src/main/java/br/com/estabulo/controller/AlunoController.java
@@ -5,39 +5,13 @@ import br.com.estabulo.dto.AlunoDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-    @RestController
-    @RequestMapping("/aluno")
-    public class AlunoController {
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
 
-        @GetMapping
-        public List<AlunoDTO> listarTodos() {
-            return new AlunosBase().listarTodos();
-        }
-
-        @GetMapping("/filtrar")
-        public List<AlunoDTO> filtrar(@RequestParam("matricula") String matricula) {
-            var alunos = new AlunosBase().listarTodos();
-            System.out.println(matricula);
-            return alunos.stream()
-                    .filter(a -> a.getMatricula().equals(matricula))
-                    .collect(Collectors.toList());
-        }
-
-        @PutMapping({"/{matricula}/{nomeAluno}"})
-        public String atualizar(@PathVariable("matricula") String matricula,
-                                @PathVariable("nomeAluno") String nomeAluno){
-            return "O aluno " + nomeAluno + " de matrícula " + matricula + " foi atualizado.";
-        }
-
-        @PostMapping
-        public String cadastrar(@RequestBody AlunoDTO aluno){
-            return "O " + aluno + " foi cadastrado";
-        }
-
-        @DeleteMapping("/{matricula}")
-        public String deletar(@PathVariable("matricula") String matricula){
-            return "O aluno de matrícula: " + matricula + " foi deletado";
-        }
+    @GetMapping
+    public List<AlunoDTO> listarTodos() {
+        return new AlunosBase().listarTodos();
     }
+}
