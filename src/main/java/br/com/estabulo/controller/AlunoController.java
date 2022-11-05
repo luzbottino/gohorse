a5ee5b236ab41aac0d8bb90c79d531b3c1919afb
Luis Bottino <luis.bottino@terceiros.sicredi.com.br>
2022-11-04 22:57:20 -0300

    primeiro endpoint get

--- src/main/java/br/com/estabulo/controller/AlunoController.java
+++ src/main/java/br/com/estabulo/controller/AlunoController.java
@@ -1,17 +1,13 @@
 package br.com.estabulo.controller;
 
-import br.com.estabulo.AlunosBase;
-import br.com.estabulo.dto.AlunoDTO;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/aluno")
 public class AlunoController {
 
-    @GetMapping
-    public List<AlunoDTO> listarTodos() {
-        return new AlunosBase().listarTodos();
+    @GetMapping("/meu-primeiro-endpoint")
+    public String meuPrimeiroEndpoint() {
+        return "Parabéns! Este é o seu primeiro endpoint GET";
     }
 }
