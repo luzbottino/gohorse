ccbd267977b8d43ed9d3e48ae2f7fa95e138e623
Luiz Augusto <luizaugustobottiino@gmail.com>
2022-11-11 23:35:15 -0300

    Configuração do spring data e do banco de dados H2.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO ALUNO (id, nome, idade) VALUES (1, 'Xaolin Matador de Porco', 21);
+INSERT INTO ALUNO (id, nome, idade) VALUES (2, 'Xaolin Matador de Galinha', 22);
+INSERT INTO ALUNO (id, nome, idade) VALUES (3, 'Xaolin Matador de Vaca', 23);
