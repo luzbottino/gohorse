10bc58a17ee645da9405ec9345191ecc850b4010
Luiz Augusto <luizaugustobottiino@gmail.com>
2022-11-25 23:09:00 -0300

    Configuração do spring data e do banco de dados H2. Criação de configuração do banco de dados e da tabela Aluno. Criação do método de consulta de Alunos.

--- src/main/java/br/com/estabulo/controller/AlunoController.java
+++ src/main/java/br/com/estabulo/controller/AlunoController.java
@@ -1,13 +1,36 @@
 package br.com.estabulo.controller;
 
+import br.com.estabulo.domain.Aluno;
+import br.com.estabulo.dto.AlunoDTO;
+import br.com.estabulo.repository.AlunoDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AlunoController {
 
-    @GetMapping("/meu-primeiro-endpoint")
-    public String meuPrimeiroEndpoint() {
-        return "Parabéns! Este é o seu primeiro endpoint GET";
+    private final AlunoDAO alunoDAO;
+
+    @Autowired
+    public AlunoController(AlunoDAO alunoDAO) {
+        this.alunoDAO = alunoDAO;
+    }
+
+    @GetMapping("/alunos")
+    public List<AlunoDTO> listarAlunos() {
+        List<Aluno> alunos = alunoDAO.findAll();
+        List<AlunoDTO> alunosDTO = new ArrayList<>();
+
+        alunos.forEach(aluno -> {
+            AlunoDTO alunoDTO = new AlunoDTO(aluno.getNome(), aluno.getMatricula(), aluno.getCreditos(), aluno.getCurso());
+            alunosDTO.add(alunoDTO);
+        });
+
+        return alunosDTO;
     }
 }
